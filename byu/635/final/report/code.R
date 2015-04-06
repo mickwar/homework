2b80a32620a93a14dc54331d06aa1b0a8c6f4c83
mickey <mickey.a.warner@gmail.com>
2015-04-05 22:45:54 -0600

    yes

--- byu/635/final/report/code.R
+++ byu/635/final/report/code.R
@@ -24,6 +24,14 @@ alpha = c(-2.5, 1, -1, -0.5)
 D1 = matrix(c(1,0,0,0), 2, 2, byrow=TRUE)
 D2 = matrix(c(0.5,0,0,0.25), 2, 2, byrow=TRUE)
 b = mvrnorm(K, c(0, 0), D1)
+c1 = 16*sqrt(3)/(15*pi)
+
+
+det(c1^2 * D1 %*% t(b) %*% b + diag(2))
+ci = double(K)
+for (i in 1:K)
+    ci[i] =  (1 + c1^2 * t(b[i,]) %*% D1 %*% b[i,])^(-1/2)
+
 b = matrix(rep(b, each = nk), K*nk, 2, byrow = F) * cbind(1, rep(tl, K))
 
 y1 = rbinom(K*nk, 1, prob = logistic(X %*% alpha + apply(b, 1, sum)))
