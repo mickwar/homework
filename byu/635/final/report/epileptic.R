1c0724cef8ae5e45950546ec022e6303cf3886d6
mickey <mickey.a.warner@gmail.com>
2015-04-10 15:06:11 -0600

    yes

--- byu/635/final/report/epileptic.R
+++ byu/635/final/report/epileptic.R
@@ -1,3 +1,4 @@
+library(MASS)
 ### glmm on epileptic data
 dat = read.table("~/files/data/epileptics.txt", header = TRUE)
 
@@ -14,10 +15,17 @@ X[,5] = rep(log(dat$Age), each=p)       # Age
 X[,6] = rep(c(0,0,0,1), n)              # Indicator for Visit 4
 
 # set up the Z matrix
-Z = cbind(kronecker(diag(n), rep(1, p)),
-    kronecker(diag(n), seq(-3, 3, by = 2)/10),
-    diag(n*p))
-dim(Z)
+# Z = cbind(kronecker(diag(n), rep(1, p)),
+#     kronecker(diag(n), seq(-3, 3, by = 2)/10),
+#     diag(n*p))
+# dim(Z)
+Z = cbind(1, rep(seq(-3, 3, by = 2)/10, n))
+Z1 = cbind(Z, 1)
+
+# set up Y
+Y = as.numeric(t(dat[,2:5]))
+Y1 = Y + 1 # for the gamma function instead of factorial
+
 
 ### wishart
 tr = function(x)
@@ -76,15 +84,193 @@ rwish2 = function(V, df){
 ### end wishart
 
 # assuming indepedence among subjects AND measurements
-calc.post = function(alpha, Ginv){
-    mu = 
-    X 
+calc.post = function(alpha, G, e, K){
+    require(MASS)
+    b12 = matrix(rep(mvrnorm(n, rep(0, 2), G), each=2), n*p, 2)
+    b3 = rnorm(n*p, 0, sqrt(e))
+    b = cbind(b12, b3)
+    lmu = X %*% alpha + apply(Z1 * b, 1, sum)
+    mu = exp(lmu)
+    D = matrix(0, 3, 3)
+    D[1:2,1:2] = G
+    D[3,3] = e
+    V = Z1 %*% D %*% t(Z1) + kronecker(diag(n), K)
+
+    # likelihood
+    out = sum(Y * lmu - mu - lgamma(Y1))
+    
+    # priors
+    out = out - 0.5*determinant(V)$modulus[1] - 0.5*t(lmu - X %*% alpha) %*% (solve(V) %*%
+        (lmu - X %*% alpha))
+    out = out + dwishart(D, rand_cov_prior, 3, log = TRUE)
+    out = out + 0.5*determinant(acovinv)$modulus[1] - 0.5*t(alpha) %*% acovinv %*% alpha
+    
+    return (out)
     }
 
 
 nburn = 100
 nmcmc = 500
 
-alpha = matrix(0, nburn + nmcmc, 6)
-Ginv = rep(list(matrix(0, ncol(Z), ncol(Z))), nburn+nmcmc)
-alpha = matrix(0, nburn + nmcmc, 6)
+alpha = matrix(0, nburn + nmcmc, ncol(X))
+G = rep(list(matrix(0, ncol(Z), ncol(Z))), nburn+nmcmc)
+e = matrix(0, nburn+nmcmc, 1)
+K = rep(list(matrix(0, p, p)), nburn+nmcmc)
+
+#alpha[1,] = rep(0, ncol(X))
+#G[[1]] = diag(2)
+#e[1,] = 1
+#K[[1]] = matrix(0.5, p, p) + 0.5*diag(p)
+
+alpha[1,] = old.alpha
+G[[1]] = old.G
+e[1,] = old.e
+K[[1]] = old.K
+#old.alpha = alpha[600,]
+#old.G = G[[600]]
+#old.e = e[600,]
+#old.K = K[[600]]
+
+a.sig = rep(0.000001, ncol(X))
+a.accept = matrix(0, nburn + nmcmc, ncol(X))
+
+g.sig = rep(0.000001, 3)
+g.accept = matrix(0, nburn + nmcmc, 3)
+
+e.sig = 0.000001
+e.accept = matrix(0, nburn + nmcmc, 1)
+
+k.sig = rep(0.000001, sum(1:p))
+k.accept = matrix(0, nburn + nmcmc, length(k.sig))
+
+alpha_cov_prior = 1e3 * solve(t(X) %*% X)
+acovinv = solve(alpha_cov_prior)
+rand_cov_prior = matrix(0.5, 3, 3) + 0.5*diag(3)
+
+post = calc.post(alpha[1,], G[[1]], e[1,], K[[1]])
+keep.post = double(nburn+nmcmc)
+keep.post[1] = post
+
+### METROPOLOIS
+for (i in 2:(nburn+nmcmc)){
+    ### update alpha
+    alpha[i,] = alpha[i-1,]
+    cand.alpha = alpha[i-1,]
+    for (j in 1:ncol(X)){
+        cand.alpha[j] = rnorm(1, cand.alpha[j], a.sig[j])
+        cand.post = calc.post(cand.alpha, G[[i-1]], e[i-1,], K[[i-1]])
+        if (log(runif(1)) < cand.post - post){
+            alpha[i,j] = cand.alpha[j]
+            a.accept[i,j] = 1
+            post = cand.post
+        } else {
+            cand.alpha[j] = alpha[i-1,j]
+            }
+        }
+
+    ### update G
+    G[[i]] = G[[i-1]]
+    cand.G = G[[i-1]]
+    # first variance
+    cand.G[1,1] = rnorm(1, cand.G[1,1], g.sig[1])
+    if (det(cand.G) > 0){
+        cand.post = calc.post(alpha[i,], cand.G, e[i-1,], K[[i-1]])
+        if (log(runif(1)) < cand.post - post){
+            G[[i]][1,1] = cand.G[1,1]
+            g.accept[i,1] = 1
+            post = cand.post
+        } else {
+            cand.G[1,1] = G[[i]][1,1]
+            }
+    } else {
+        cand.G[1,1] = G[[i]][1,1]
+        }
+    # second variance
+    cand.G[2,2] = rnorm(1, cand.G[2,2], g.sig[2])
+    if (det(cand.G) > 0){
+        cand.post = calc.post(alpha[i,], cand.G, e[i-1,], K[[i-1]])
+        if (log(runif(1)) < cand.post - post){
+            G[[i]][2,2] = cand.G[2,2]
+            g.accept[i,2] = 1
+            post = cand.post
+        } else {
+            cand.G[2,2] = G[[i]][2,2]
+            }
+    } else {
+        cand.G[2,2] = G[[i]][2,2]
+        }
+    # covariance
+    cand.G[1,2] <- cand.G[2,1] <-  rnorm(1, cand.G[1,2], g.sig[3])
+    if (det(cand.G) > 0){
+        cand.post = calc.post(alpha[i,], cand.G, e[i-1,], K[[i-1]])
+        if (log(runif(1)) < cand.post - post){
+            G[[i]][1,2] <- G[[i]][2,1] <- cand.G[1,2]
+            g.accept[i,3] = 1
+            post = cand.post
+        } else {
+            cand.G[1,2] <- cand.G[2,1] <- G[[i]][1,2]
+            }
+    } else {
+        cand.G[1,2] <- cand.G[2,1] <- G[[i]][1,2]
+        }
+
+    ### update e
+    e[i,] = e[i-1,]
+    cand.e = rnorm(1, e[i-1,], e.sig)
+    if (cand.e > 0){
+        cand.post = calc.post(alpha[i,], G[[i]], cand.e, K[[i-1]])
+        if (log(runif(1)) < cand.post - post){
+            e[i,] = cand.e
+            e.accept[i,1] = 1
+            post = cand.post
+            }
+        }
+
+    ### update K
+    K[[i]] = K[[i-1]]
+    cand.K = K[[i-1]]
+    # variances
+    for (j in 1:p){
+        cand.K[j,j] = rnorm(1, cand.K[j,j], k.sig[2])
+        if (det(cand.K) > 0){
+            cand.post = calc.post(alpha[i,], G[[i]], e[i,], cand.K)
+            if (log(runif(1)) < cand.post - post){
+                K[[i]][j,j] = cand.K[j,j]
+                k.accept[i,j] = 1
+                post = cand.post
+            } else {
+                cand.K[j,j] = K[[i]][j,j]
+                }
+        } else {
+            cand.K[j,j] = K[[i]][j,j]
+            }
+        }
+    # covariances
+    count = 0
+    for (j in 1:(p-1)){
+        for (k in (j+1):p){
+            count = count + 1
+            cand.K[j,k] <- cand.K[k,j] <-  rnorm(1, cand.K[j,k], k.sig[p+count])
+            if (det(cand.K) > 0){
+                cand.post = calc.post(alpha[i,], G[[i]], e[i,], cand.K)
+                if (log(runif(1)) < cand.post - post){
+                    K[[i]][j,k] <- K[[i]][k,j] <- cand.K[j,k]
+                    k.accept[i,p+count] = 1
+                    post = cand.post
+                } else {
+                    cand.K[j,k] <- cand.K[k,j] <- K[[i]][j,k]
+                    }
+            } else {
+                cand.K[j,k] <- cand.K[k,j] <- K[[i]][j,k]
+                }
+            }
+        }
+
+    keep.post[i] = post
+    }
+
+
+plot(keep.post, type='l')
+
+accept = cbind(a.accept, g.accept, e.accept, k.accept)
+apply(accept, 2, mean)
