97e3db740809da162ffa327944bfa116af72d06a
mickey <mickey.a.warner@gmail.com>
2015-11-19 21:47:06 -0800

    yes

--- santa_cruz/241/hw/2/prob3c.R
+++ santa_cruz/241/hw/2/prob3c.R
@@ -15,7 +15,7 @@ pplc = function(y, ypred, k = Inf){
     }
 
 dat = read.table("~/files/data/fabric.txt", header = TRUE)
-x = dat$length / 100
+x = as.numeric(scale(dat$length))
 y = dat$faults
 n = length(y)
 ord = order(y)
@@ -41,8 +41,8 @@ rinvgamma = function(n, shape, rate)
 
 
 ### MCMC
-nburn = 200
-nmcmc = 1000
+nburn = 5000
+nmcmc = 10000
 
 # Parameter objects
 param.theta = matrix(0, nburn + nmcmc, n)
@@ -50,15 +50,23 @@ param.alpha = double(nburn + nmcmc)
 param.psi = matrix(0, nburn + nmcmc, 2) # [,1] is zeta, [,2] is mu
 param.beta = double(nburn + nmcmc)
 
+# other mcmc
+sig.beta = 0.01
+acc.beta = double(nburn + nmcmc)
+sig.psi = diag(0.01, 2)
+acc.psi = double(nburn + nmcmc)
+
+window = 100
+
 # Priors
 prior.beta.a  = 0   # Normal (mean)
 prior.beta.b  = 1   # Normal (sd)
 prior.alpha.a = 1   # Gamma (shape)           alpha controls n.star (discreteness of G)
-prior.alpha.b = 1   # Gamma (rate)
-prior.zeta.a  = 1   # Normal (mean)
-prior.zeta.b  = 1   # Normal (variance)
-prior.mu.a    = 1   # Gamma (shape)
-prior.mu.b    = 1   # Gamma (rate)
+prior.alpha.b = 1/2 # Gamma (rate)
+prior.zeta.a  = 4   # Gamma (shape)
+prior.zeta.b  = 1/4 # Gamma (rate)
+prior.mu.a    = 4   # Gamma (shape)
+prior.mu.b    = 1/4 # Gamma (rate)
 
 
 # Initial values
@@ -67,10 +75,9 @@ param.beta[1] = rnorm(1, prior.beta.a, prior.beta.b)
 param.alpha[1] = rgamma(1, prior.alpha.a, prior.alpha.b)
 param.psi[1,] = c(rgamma(1, prior.zeta.a, prior.zeta.b),
     rgamma(1, prior.mu.a, prior.mu.b))
+#param.theta[1,] = rep(0, n)
+param.theta[1,] = y
 
-##############
-#### HERE HERE HERE
-##############
 
 # Iterations
 for (iter in 2:(nburn + nmcmc)){
@@ -79,9 +86,9 @@ for (iter in 2:(nburn + nmcmc)){
     # Current parameters (used for convenience)
     c.theta = param.theta[iter-1,]
     c.alpha = param.alpha[iter-1]
-    c.mu = param.psi[iter-1,1]
-    c.tau2 = param.psi[iter-1,2]
-    c.phi = param.phi[iter-1]
+     c.beta = param.beta[iter-1]
+     c.zeta = param.psi[iter-1,1]
+       c.mu = param.psi[iter-1,2]
 
     # Update thetas
     for (i in 1:n){
@@ -89,13 +96,12 @@ for (iter in 2:(nburn + nmcmc)){
         theta.star.minus = as.numeric(names(n.j.minus))
         n.star.minus = length(theta.star.minus)
 
-        # q0 here is a normal density (after annoying integration)
-        q0 = dnorm(y[i], c.mu, sqrt(c.phi + c.tau2))
-
-        #dnbinom
+        # q0 is negative binomial
+        temp.p = (c.zeta / c.mu) / (exp(c.beta*x[i]) + c.zeta / c.mu)
+        q0 = dnbinom(y[i], size = c.zeta, prob = temp.p)
 
         # as is qj, by construction
-        qj = dnorm(y[i], theta.star.minus, sqrt(c.phi))
+        qj = dpois(y[i], theta.star.minus*exp(c.beta*x[i]))
 
         # Probabilities of determining which to draw
         # Calculate A
@@ -107,8 +113,7 @@ for (iter in 2:(nburn + nmcmc)){
         # Make the update
         draw = sample(n.star.minus + 1, 1, prob = c(A, Bj))
         if (draw == 1){ # Make a draw from h
-            c.theta[i] = rnorm(1, (c.mu*c.phi + y[i]+c.tau2)/(c.phi + c.tau2),
-                sqrt(c.phi*c.tau2/(c.phi + c.tau2)))
+            c.theta[i] = rgamma(1, y[i] + c.zeta, exp(c.beta*x[i]) + c.zeta/c.mu)
         } else { # Make a draw from the existing groups
             c.theta[i] = theta.star.minus[draw-1]
             }
@@ -116,6 +121,33 @@ for (iter in 2:(nburn + nmcmc)){
     n.j = table(c.theta)
     theta.star = as.numeric(names(n.j))
     n.star = length(theta.star)
+
+    # Update beta
+    cand = rnorm(1, c.beta, sig.beta)
+    temp.curr = dnorm(c.beta, prior.beta.a, prior.beta.b, log = TRUE) +
+        sum(dpois(y, c.theta*exp(c.beta*x), log = TRUE))
+    temp.cand = dnorm(cand, prior.beta.a, prior.beta.b, log = TRUE) +
+        sum(dpois(y, c.theta*exp(cand*x), log = TRUE))
+    if (log(runif(1)) < temp.cand - temp.curr){
+        c.beta = cand
+        acc.beta[iter] = 1
+        }
+
+    # Update psi = (zeta, mu)
+    cand = mvrnorm(1, c(c.zeta, c.mu), sig.psi)
+    if (all(cand > 0)){
+        temp.curr = dgamma(c.zeta, prior.zeta.a, prior.zeta.b, log = TRUE) +
+            dgamma(c.mu, prior.mu.a, prior.mu.b, log = TRUE) +
+            sum(dgamma(theta.star, c.zeta, c.zeta / c.mu, log = TRUE))
+        temp.cand = dgamma(cand[1], prior.zeta.a, prior.zeta.b, log = TRUE) +
+            dgamma(cand[2], prior.mu.a, prior.mu.b, log = TRUE) +
+            sum(dgamma(theta.star, cand[1], cand[1] / cand[2], log = TRUE))
+        if (log(runif(1)) < temp.cand - temp.curr){
+            c.zeta = cand[1]
+            c.mu = cand[2]
+            acc.psi[iter] = 1
+            }
+        }
 
     # Update alpha
     # Use an auxiliary variable eta to draw a new alpha
@@ -128,31 +160,29 @@ for (iter in 2:(nburn + nmcmc)){
         c.alpha = rgamma(1, prior.alpha.a + n.star - 1, prior.alpha.b - log(eta))
         }
 
-    # Update psi = (mu, tau^2)
-    S = sum(theta.star)
-    c.mu = rnorm(1, (prior.mu.mean*c.tau2 + prior.mu.var * S) /
-        (c.tau2 + prior.mu.var * n.star),
-        sqrt(c.tau2 * prior.mu.var / (c.tau2 + prior.mu.var * n.star)))
-    c.tau2 = rinvgamma(1, prior.tau2.a + n.star/2,
-        prior.tau2.b + 1/2 * sum((theta.star - c.mu)^2))
-
-    # Update phi
-    c.phi = rinvgamma(1, prior.phi.a + n/2, prior.phi.b + 1/2 * sum((y - c.theta)^2))
-
     # Extra improvement step (only updating the clusters, not individuals)
     for (j in 1:n.star){
         temp.w = which(c.theta == theta.star[j])
-        c.theta[temp.w] = rnorm(1,
-            (c.phi*c.mu + c.tau2*sum(y[temp.w])) / (c.phi + n.j[j]*c.tau2),
-            sqrt(c.phi*c.tau2 / (c.phi + n.j[j]*c.tau2)))
+        c.theta[temp.w] = rgamma(1,
+            c.zeta + sum(y[temp.w]),
+            c.zeta / c.mu + sum(exp(c.beta*x[temp.w])))
         }
 
     # Put the c.* objects into the regular ones
     param.theta[iter,] = c.theta
     param.alpha[iter] = c.alpha
-    param.psi[iter,1] = c.mu
-    param.psi[iter,2] = c.tau2
-    param.phi[iter] = c.phi
+    param.beta[iter] = c.beta
+    param.psi[iter,1] = c.zeta
+    param.psi[iter,2] = c.mu
+
+    # Improve candidate sigmas
+    if ((floor(iter/window) == iter/window) && (iter <= nburn)){
+        sig.beta = autotune(mean(acc.beta[(iter-window+1):iter]),
+            target = 0.25, k = window/50) * sig.beta
+        sig.psi = autotune(mean(acc.psi[(iter-window+1):iter]),
+            target = 0.25, k = window/50) *
+            (sig.psi + window * var(param.psi[(iter-window+1):iter,]) / iter)
+        }
 
     if (iter == (nburn + nmcmc))
         cat("\n")
@@ -161,8 +191,15 @@ for (iter in 2:(nburn + nmcmc)){
 ### Burn in
 param.theta = tail(param.theta, nmcmc)
 param.alpha = tail(param.alpha, nmcmc)
-param.phi = tail(param.phi, nmcmc)
+param.beta = tail(param.beta, nmcmc)
 param.psi = tail(param.psi, nmcmc)
+
+acc.beta = tail(acc.beta, nmcmc)
+acc.psi = tail(acc.psi, nmcmc)
+
+mean(acc.beta)
+mean(acc.psi)
+
 
 
 ### Individual plots for the theta's
@@ -177,21 +214,13 @@ param.psi = tail(param.psi, nmcmc)
 #    }
 
 ### "Trace plot" for clusters
-#pdf("./figs/improve_comp.pdf", height = 9, width = 9)
-#par(mfrow = c(1,1), mar = c(3.1, 4.1, 2.1, 1.1))
-#plot(0, type='n', xlim = c(1, nrow(pback)), ylim = range(pback),
-#    xlab = "MCMC Iteration", ylab = "Unique cluster locations", main = "Without improving Gibbs step")
-#for (i in 1:nrow(pback)){
-#    unq = unique(pback[i,])
-#    points(rep(i, length(unq)), unq, pch = 20, cex = 0.2)
-#    }
-#plot(0, type='n', xlim = c(1, nrow(param.theta)), ylim = range(param.theta),
-#    xlab = "MCMC Iteration", ylab = "Unique cluster locations", main = "With improving Gibbs step")
-#for (i in 1:nrow(param.theta)){
-#    unq = unique(param.theta[i,])
-#    points(rep(i, length(unq)), unq, pch = 20, cex = 0.2)
-#    }
-#dev.off()
+par(mfrow = c(1,1), mar = c(3.1, 4.1, 2.1, 1.1))
+plot(0, type='n', xlim = c(1, nrow(param.theta)), ylim = range(param.theta),
+    xlab = "MCMC Iteration", ylab = "Unique cluster locations")
+for (i in 1:nrow(param.theta)){
+    unq = unique(param.theta[i,])
+    points(rep(i, length(unq)), unq, pch = 20, cex = 0.2)
+    }
 
 ### Box plots of the theta's
 qlines = apply(param.theta, 2, quantile, c(0.025, 0.975))
@@ -211,10 +240,10 @@ lines(mline[ord], col = 'darkgreen', lwd = 1.5)
 
 #pdf("./figs/lines_1.pdf", width = 8, height = 8)
 par(mfrow = c(1,1), mar = c(4.1, 2.1, 2.1, 1.1))
-plot(1:250, type='n', xlim = c(1, 250), ylim = c(range(y)),
+plot(1:n, type='n', ylim = c(range(y)),
     xlab = expression(theta ~ "index"), cex.lab = 2,
     main = expression("Posterior cluster locations for each" ~ theta[i]), cex.main = 2)
-segments(x0 = 1:250, y0 = qlines[1,ord], y1 = qlines[2,ord], lwd = 1.0)
+segments(x0 = 1:n, y0 = qlines[1,ord], y1 = qlines[2,ord], lwd = 1.0)
 lines(mline[ord], col = 'darkgreen', lwd = 3.0)
 points(y[ord], col = 'red', pch = 20, cex = 0.5)
 legend("topleft", box.lty = 0, legend = c("Data (ordered)", "Mean", "95% credible interval"),
@@ -259,33 +288,21 @@ hpd.plot(density(param.alpha), hpd.alpha, main = expression(alpha), xlab = "", c
 #dev.off()
 
 
-#table(clusters) / nmcmc
-
-#plot(clusters, type='l')
-#abline(h=3, lty=2)
-
-#cback = apply(pback, 1, function(x) length(unique(x)))
-#lines(as.numeric(names(table(cback)))+0.2, table(cback) / nmcmc, main = expression(n^"*"), cex.main = 2, col = 'red', type='h', lwd = 3)
-#table(cback) / nmcmc
-
-#plot(density(apply(param.theta[clusters == 3,], 1, unique)))
-#x  =apply(param.theta[clusters == 3,], 1, unique)
-
 ### Posterior for alpha, phi, mu, tau^2
-hpd.phi = hpd.uni(param.phi)
-hpd.mu = hpd.uni(param.psi[,1])
-hpd.tau2 = hpd.uni(param.psi[,2])
+hpd.beta = hpd.uni(param.beta)
+hpd.zeta = hpd.uni(param.psi[,1])
+hpd.mu = hpd.uni(param.psi[,2])
 
 #pdf("./figs/posts_1.pdf", height = 8, width = 8)
 par(mfrow = c(3,2), mar = c(3.1, 2.1, 2.1, 1.1), oma = c(0,0,2,0))
-plot(param.phi, type='l', main = "Traceplot")
-hpd.plot(density(param.phi), hpd.phi, main = expression(phi), xlab="", cex.main = 2.0)
+plot(param.beta, type='l', main = "Traceplot")
+hpd.plot(density(param.beta), hpd.beta, main = expression(beta), xlab="", cex.main = 2.0)
 
 plot(param.psi[,1], type='l', main = "Traceplot")
-hpd.plot(density(param.psi[,1]), hpd.mu, main = expression(mu), xlab="", cex.main = 2)
+hpd.plot(density(param.psi[,1]), hpd.zeta, main = expression(zeta), xlab="", cex.main = 2)
 
 plot(param.psi[,2], type='l', main = "Traceplot")
-hpd.plot(density(param.psi[,2]), hpd.tau2, main = expression(tau^2), xlab="", cex.main = 2)
+hpd.plot(density(param.psi[,2]), hpd.mu, main = expression(mu), xlab="", cex.main = 2)
 title(main = "Posterior distributions (with 95% hpd set)", outer = TRUE, cex.main = 2)
 #dev.off()
 
@@ -329,78 +346,49 @@ for (i in 1:nmcmc){
     prob = c(param.alpha[i] / (param.alpha[i] + n), n_j[[i]] / (param.alpha[i] + n))
     draw = sample(length(prob), 1, replace = FALSE, prob = prob)
     if (draw == 1){
-        theta_0[i] = rnorm(1, param.psi[i, 1], sqrt(param.psi[i, 2]))
+        theta_0[i] = rgamma(1, param.psi[i, 1], param.psi[i,1] / param.psi[i, 2])
     } else {
         theta_0[i] = theta_star[[i]][draw - 1]
         }
     if (i == nmcmc)
         cat("\n")
     }
-y_0 = rnorm(nmcmc, theta_0, sqrt(param.phi))
+y_0 = matrix(0, nmcmc, n)
+for (i in 1:n)
+    y_0[,i] = rpois(nmcmc, theta_0*exp(param.beta*x[i]))
 
 hpd.theta_0 = hpd.mult(theta_0, density(theta_0))
-hpd.y_0 = hpd.mult(y_0, density(y_0))
+#hpd.y_0 = hpd.mult(y_0, density(y_0))
 
 #pdf("./figs/pred_1.pdf", height = 6, width = 12)
-par(mfrow = c(1, 2), mar = c(3.1, 2.1, 2.1, 1.1))
+par(mfrow = c(1, 1), mar = c(3.1, 2.1, 2.1, 1.1))
 hpd.plot(density(theta_0), hpd.theta_0, main = expression("Posterior predictive for" ~ theta[0] ~ "(with 95% hpd set)"), cex.main = 1, xlab = expression(theta[0]))
 
-# A new data point (just need theta_0 and phi)
-#par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1))
-hpd.plot(density(y_0), hpd.y_0, main = expression("Posterior predictive for" ~ y[0] ~ "(with 95% hpd set)"), cex.main = 1, xlab = expression(y[0]))
-lines(density(y), lwd =3)
-legend("topleft", box.lty = 0, legend = "Data", lwd = 3, cex = 1.5)
-#dev.off()
+# # A new data point (just need theta_0 and phi)
+# #par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1))
+# hpd.plot(density(y_0), hpd.y_0, main = expression("Posterior predictive for" ~ y[0] ~ "(with 95% hpd set)"), cex.main = 1, xlab = expression(y[0]))
+# lines(density(y), lwd =3)
+# legend("topleft", box.lty = 0, legend = "Data", lwd = 3, cex = 1.5)
+# #dev.off()
 
-### Prior predictive
-library(MCMCpack)
-# Cluster
 
-prior.theta_0 = double(20000)
-prior.y_0 = double(length(prior.theta_0))
-tvec = seq(-10, 10, length = 1500)
-k = length(tvec)
-for (i in 1:length(prior.y_0)){
-    cat("\r", i, "/", length(prior.y_0))
-    temp.phi = rinvgamma(1, prior.phi.a, prior.phi.b)
-    temp.alpha = rgamma(1, prior.alpha.a, prior.alpha.b)
-    temp.mu = rnorm(1, prior.mu.mean, sqrt(prior.mu.var))
-    temp.tau = sqrt(rinvgamma(1, prior.tau2.a, prior.tau2.b))
-    dG0 = pnorm(tvec, temp.mu, temp.tau) - pnorm(c(-Inf, tvec[-k]), temp.mu, temp.tau)
-    flag = TRUE
-    while (flag){
-        flag = FALSE
-        dG = rdirichlet(1, temp.alpha * dG0)
-        if (any(is.na(dG)))
-            flag = TRUE
-        }
-    keep = which(dG != 0)
-    if (length(keep) == 1){
-        prior.theta_0[i] = tvec[keep]
-    } else {
-        prior.theta_0[i] = sample(tvec[keep], 1, prob = dG[keep])
-        }
-    prior.y_0[i] = rnorm(1, prior.theta_0[i], sqrt(temp.phi))
-    if (i == length(prior.y_0))
-        cat("\n")
-    }
+### predictions
+q0 = apply(y_0, 2, quantile, c(0.025, 0.975))
+m0 = apply(y_0, 2, mean)
 
-pdf("./figs/prior_1.pdf", height = 6, width = 12)
-par(mfrow = c(1,2), mar = c(3.1, 2.1, 2.1, 1.1))
-plot(density(prior.theta_0[1:i]), main = expression("Prior predictive for" ~ theta[0]), lwd = 2,
-    col = 'blue')
-#curve(dnorm(x, mean(prior.theta_0[1:i]), sd(prior.theta_0[1:i])), add = TRUE, col = 'red', lwd = 2)
-legend("topleft", legend = "KDE of draws", box.lwd = 0, box.lty = 0, col = 'blue', lwd = 2, cex = 1.3)
-#legend("topleft", legend = c("KDE of draws", "Approximate normal"),
-#    box.lwd = 0, box.lty = 0, col = c('blue', 'red'), lwd = 2, cex = 1.3)
+### Predictions at each x_i
+par(mfrow = c(2,1), mar = c(3.1,2.1,2.1,1.1))
+plot(x, y, pch = 1, ylim = range(c(y, q0)), lwd = 1.5)
+segments(x0 = x, y0 = q0[1,], y1 = q0[2,], col = 'forestgreen')
+points(x, m0, col = 'darkgreen', pch = 20)
 
-plot(density(prior.y_0[1:i]), main = expression("Prior predictive for" ~ y[0]), lwd = 2,
-    col = 'blue')
-#curve(dnorm(x, mean(prior.y_0[1:i]), sd(prior.y_0[1:i])), add = TRUE, col = 'red', lwd = 2)
-lines(density(y), lwd =3)
-legend("topleft", legend = c("KDE of draws", "Data"),
-    box.lwd = 0, box.lty = 0, col = c('blue', 'black'), lwd = 2, cex = 1.3)
-#legend("topleft", legend = c("KDE of draws", "Approximate normal", "Data"),
-#    box.lwd = 0, box.lty = 0, col = c('blue', 'red', 'black'), lwd = 2, cex = 1.3)
-dev.off()
+### Observed vs. Fitted plot
+plot(0, type='n', xlim = range(y), ylim = range(c(y, q0)),
+    xlab = "Observed", ylab = "Fitted")
+segments(x0 = jity, y0 = q0[1,], y1 = q0[2,], col = 'forestgreen')
+points(jity, m0, col = 'darkgreen', pch = 20)
+abline(0, 1, lwd = 3, lty = 2)
+
+pplc(y, y_0, 0)
+pplc(y, y_0, Inf)
 
