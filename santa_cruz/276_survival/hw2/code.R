3bf18f1ac470fcaef613c84b730fc9f5aec24205
mickey <mickey.a.warner@gmail.com>
2016-11-07 15:56:04 -0800

    yes

--- santa_cruz/276_survival/hw2/code.R
+++ santa_cruz/276_survival/hw2/code.R
@@ -1,4 +1,4 @@
-### Problem 3
+### Proportional hazards model
 library(KMsurv)
 library(survival)
 library(mwBASE)
@@ -7,9 +7,7 @@ library(xtable)
 data(tongue)
 tongue$time = tongue$time / 10
 
-aneuploid = tongue[tongue$type == 1,]
-diploid = tongue[tongue$type == 2,]
-
+### Weibull baseline hazard
 calc.post = function(y, p){
     alpha = p[1]
     gamma = p[2]
@@ -53,4 +51,87 @@ var(beta0)
 var(beta1)
 var(sigma)
 
-mean(mod1$params[,2]/exp(mod1$params[,3]))
+
+
+### Piece-wise constant hazards
+ss = c("0%"=0, quantile(tongue[,2], seq(0.05, 1.00, by = 0.05)))
+#ss = c("0%"=0, quantile(tongue[,2], seq(0.10, 0.90, by = 0.10)), max(tongue[,2]+0.01))
+#ss = seq(0, 40, length = 50)
+J = length(ss)-1
+
+# which interval each observation (failed or censored) is in
+ints = sapply(tongue[,2], function(x) which.max(x <= ss[-1]))
+
+
+calc.post = function(y, p){
+    beta0 = p[1]
+    beta1 = p[2]
+    lambda = p[-c(1,2)]
+
+    if (any(lambda <= 0))
+        return (-Inf)
+
+    x = 2 - y[,1]
+    t = y[,2]
+    A = which(y[,3] == 1)   # set of observations where not censored
+    H = c(0, cumsum(lambda*(ss[-1] - ss[-(J+1)])))    # cumulative hazard
+
+    # Likelihood
+    out = sum(log(lambda[ints[A]]) + beta0 + x[A]*beta1) -
+        sum((lambda[ints]*(t - ss[ints]) + H[ints])*exp(beta0 + x * beta1))
+
+    # Priors
+    out = out + dnorm(beta0, 0, 10, log = TRUE)
+    out = out + dnorm(beta1, 0, 10, log = TRUE)
+    out = out + sum(dgamma(lambda, 1, 1/10, log = TRUE))
+
+    return (out)
+    }
+
+mod2 = mcmc_sampler(tongue, calc.post, nparam = (2 + J), nburn = 50000, nmcmc = 20000)
+
+mean(mod2$accept)
+plot(mod2$params[,1], type='l')
+plot(mod2$params[,2], type='l')
+plot(mod2$params[,3], type='l')
+plot(mod2$params[,4], type='l')
+
+colMeans(mod2$params)
+
+
+# for (i in 1:NCOL(mod2$params)){
+#     plot(mod2$params[,i], type='l')
+#     readline()
+#     }
+
+dip.survival = t(apply(mod2$params,1,function(p) exp(-exp(p[1])*cumsum(p[-c(1,2)]*(ss[-1]-ss[-(J+1)])))))
+ane.survival = t(apply(mod2$params,1,function(p) exp(-exp(p[1]+p[2])*cumsum(p[-c(1,2)]*(ss[-1]-ss[-(J+1)])))))
+
+dip.mm = apply(dip.survival, 2, mean)
+dip.vv = apply(dip.survival, 2, quantile, c(0.025, 0.975))
+ane.mm = apply(ane.survival, 2, mean)
+ane.vv = apply(ane.survival, 2, quantile, c(0.025, 0.975))
+
+# Kaplan-Meier curves
+plot(survfit(Surv(time[type == 1], delta[type == 1]) ~ 1, data = tongue), col = 'dodgerblue', lwd=1)
+lines(survfit(Surv(time[type == 2], delta[type == 2]) ~ 1, data = tongue), col = 'firebrick1', lwd=1)
+
+# Posterior survival curves
+plot(0, type='n', xlim = range(ss), ylim = c(0, 1), bty = 'n')
+lines(ss, c(1, dip.mm), col = 'firebrick1', lwd = 3)
+lines(ss, c(1, dip.vv[1,]), col = 'firebrick1',)
+lines(ss, c(1, dip.vv[2,]), col = 'firebrick1',)
+lines(ss, c(1, ane.mm), col = 'dodgerblue', lwd = 3)
+lines(ss, c(1, ane.vv[1,]), col = 'dodgerblue',)
+lines(ss, c(1, ane.vv[2,]), col = 'dodgerblue',)
+
+# Together
+plot(survfit(Surv(time[type == 1], delta[type == 1]) ~ 1, data = tongue), col = 'dodgerblue')
+lines(survfit(Surv(time[type == 2], delta[type == 2]) ~ 1, data = tongue), col = 'firebrick1')
+lines(ss, c(1, dip.mm), col = 'firebrick1', lwd = 3)
+lines(ss, c(1, dip.vv[1,]), col = 'firebrick1',)
+lines(ss, c(1, dip.vv[2,]), col = 'firebrick1',)
+lines(ss, c(1, ane.mm), col = 'dodgerblue', lwd = 3)
+lines(ss, c(1, ane.vv[1,]), col = 'dodgerblue',)
+lines(ss, c(1, ane.vv[2,]), col = 'dodgerblue',)
+
