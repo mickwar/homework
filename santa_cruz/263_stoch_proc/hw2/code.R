8796fba09c2f2122cd60471b10be639dd7494871
mickey <mickey.a.warner@gmail.com>
2017-02-03 13:10:37 -0800

    yes

--- santa_cruz/263_stoch_proc/hw2/code.R
+++ santa_cruz/263_stoch_proc/hw2/code.R
@@ -17,14 +17,14 @@ plot(ss, L %*% z, pch = 16, type='o')
 ### No 2
 library(MASS)
 set.seed(1)
-n = 100
+n = 1000
 x = rnorm(n)
 sig = sqrt(0.01)
 y = 0.3 + 0.4*x + 0.5*sin(2.7*x) + 1.1/(1+x^2) + rnorm(n, 0, sig)
 plot(x, y)
 
 # GP with exponential correlation function
-B = 50
+B = 1000
 post.sig2 = double(B)+1
 post.tau2 = double(B)+1
 post.phi = double(B)+1
@@ -35,25 +35,28 @@ Dmat = as.matrix(dist(x, diag = TRUE, upper = TRUE))
 H.phi = exp(-post.phi[1]*Dmat)
 U.phi = chol(H.phi)
 U.inv = backsolve(U.phi, diag(n))
-f.phi.post = function(phi, f, mu, U.inv){
-    if (phi <= 0 || phi >= 50)
-        return (-Inf)
-    return (sum(log(diag(U.inv)))-1/2*sum((t(f - mu) %*% U.inv)^2))
-    }
+f.phi.post = function(phi, f, mu, U.inv)
+    sum(log(diag(U.inv)))-1/2*sum((t(f - mu) %*% U.inv)^2)
 
 for (b in 5:B){
 
     ll = f.phi.post(post.phi[b-1], post.f[b-1,], post.mu[b-1], U.inv)
     cand.phi = rnorm(1, post.phi[b-1], 0.5)
-    cand.H.phi = exp(-cand.phi*Dmat)
-    cand.U.phi = chol(cand.H.phi)
-    cand.U.inv = backsolve(cand.U.phi, diag(n))
-    ll.cand = f.phi.post(cand.phi, post.f[b-1,], post.mu[b-1], cand.U.inv)
-    if (log(runif(1)) <= ll.cand - ll){
-        post.phi[b] = cand.phi
-        accept.phi[b] = 1
-        H.phi = cand.H.phi
-        U.inv = cand.U.inv
+    if (cand.phi > 0 && cand.phi <= 50){
+        cand.H.phi = exp(-cand.phi*Dmat)
+        cand.U.phi = chol(cand.H.phi)
+        cand.U.inv = backsolve(cand.U.phi, diag(n))
+        ll.cand = f.phi.post(cand.phi, post.f[b-1,], post.mu[b-1], cand.U.inv)
+        if (log(runif(1)) <= ll.cand - ll){
+            post.phi[b] = cand.phi
+            accept.phi[b] = 1
+            H.phi = cand.H.phi
+            U.inv = cand.U.inv
+        } else {
+            post.phi[b] = post.phi[b-1]
+            }
+    } else {
+        post.phi[b] = post.phi[b-1]
         }
 
     post.sig2[b] = 1/rgamma(1, 1 + n/2, 1 + 1/2*sum((y - post.f[b-1,])^2))
@@ -71,6 +74,15 @@ for (b in 5:B){
     points(x, y, col= 'red', pch = 16)
 
     }
+
+
+pp = apply(post.f, 2, mean)
+qq = apply(post.f, 2, quantile, c(0.025, 0.5, 0.975))
+
+plot(x, y, pch = 16, ylim = range(qq))
+lines(sort(x), pp[order(x)], col = 'blue', lwd = 3)
+lines(sort(x), qq[1, order(x)], col = 'blue', lwd = 1)
+lines(sort(x), qq[3, order(x)], col = 'blue', lwd = 1)
 
 
 
