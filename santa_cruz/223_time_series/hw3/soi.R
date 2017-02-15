4bf8dd34e2bb100bc3721c8a10fb97cac4398756
mickey <mickey.a.warner@gmail.com>
2017-02-15 14:28:54 -0800

    yes

--- santa_cruz/223_time_series/hw3/soi.R
+++ santa_cruz/223_time_series/hw3/soi.R
@@ -13,6 +13,39 @@
 # time series models, or requires models that include drifts/trends 
 # that may be related to global climatic change. 
 
-dat = read.table("~/files/repos/data/soi.txt")[,1]
+tmp = read.table("~/files/repos/data/soi.txt")[,1]
+tmp = diff(c(co2))
 
-plot(dat, type='l')
+dat = NULL
+dat$tt = seq(1, length(tmp), by = 1)
+dat$y = tmp
+
+plot(dat$tt, dat$y, type='l', bty='n')
+
+calc.post = function(omega, dat){
+    tt = dat$tt
+    y = dat$y
+    n = length(y)
+    Fm = rbind(cos(omega * tt), sin(omega * tt)) # = F'
+    beta.hat = solve(Fm %*% t(Fm)) %*% Fm %*% y
+
+#   exp((-0.5)*determinant(Fm %*% t(Fm))$modulus[1]) *
+#       (1 - (t(beta.hat) %*% Fm %*% t(Fm) %*% beta.hat) / sum(y^2))^((2 - n)/2)
+
+    (-0.5)*determinant(Fm %*% t(Fm))$modulus[1] +
+        ((2-n)/2)*log(1 - (t(beta.hat) %*% Fm %*% t(Fm) %*% beta.hat) / sum(y^2))
+
+    }
+
+omega = seq(0.001, pi - 0.001, length = 1000)
+lambda = 2*pi / omega
+
+tp = sapply(omega, function(x) calc.post(x, dat))
+
+tp = sapply(lambda, function(x) calc.post(2*pi/x, dat)-2*log(x))
+
+plot(omega, tp, type='l')
+
+plot(lambda, tp, type='l', xlim = c(2, 100))
+
+calc.post(0.001, dat)
