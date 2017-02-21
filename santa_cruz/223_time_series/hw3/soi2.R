c6aea3bcc48ba2a73cc1b92b90f881a45c9e077e
mickey <mickey.a.warner@gmail.com>
2017-02-20 16:31:20 -0800

    yes

--- santa_cruz/223_time_series/hw3/soi2.R
+++ santa_cruz/223_time_series/hw3/soi2.R
@@ -1,6 +1,13 @@
-tmp = read.table("~/files/repos/data/soi.txt")[,1]
-#tmp = lh
+#tmp = read.table("~/files/repos/data/soi.txt")[,1]
+tmp = lh
 #tmp = diff(c(co2))
+
+#tmp = rnorm(4000)
+
+#tmp = sin(seq(0, 1, length=1000)*2*pi * 4*10) +
+#    sin(seq(0, 1, length=1000)*2*pi * 7*10) +
+#    sin(seq(0, 1, length=1000)*2*pi * 18*10) +
+#    sin(seq(0, 1, length=1000)*2*pi * 29*10)
 
 dat = NULL
 dat$tt = seq(1, length(tmp), by = 1)
@@ -10,35 +17,38 @@ y = dat$y
 tt = dat$tt
 n = length(y)
 
-plot(dat$tt, dat$y, type='l', bty='n')
+#par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
+# plot(dat$tt, dat$y, type='l', bty='n')
 
 k = 1:floor(n/2-1/2)
+omega = 2*pi*k/n
 
-omega = expand.grid(2*pi*k/n, 2*pi*k/n)
+# omega = seq(0.001, pi-0.001, length = 1000)
 
-omega.1 = 2*pi*k/n
-omega.2 = 2*pi*l/n
+a.hat = (2/n)*sapply(omega, function(w) sum(y * cos(w * tt)))
+b.hat = (2/n)*sapply(omega, function(w) sum(y * sin(w * tt)))
 
-a.hat.1 = (2/n)*sapply(omega[,1], function(w) sum(y * cos(w * tt)))
-b.hat.1 = (2/n)*sapply(omega[,1], function(w) sum(y * sin(w * tt)))
-a.hat.2 = (2/n)*sapply(omega[,2], function(w) sum(y * cos(w * tt)))
-b.hat.2 = (2/n)*sapply(omega[,2], function(w) sum(y * sin(w * tt)))
+Iw = (n/2)*(a.hat^2 + b.hat^2)
+pw = (1 - Iw/sum(y^2))^(1 - n/2)
 
-Iw = (n/2)*(a.hat.1^2 + b.hat.1^2 + a.hat.2^2 +b.hat.2^2)
 
-plot(omega[,1], (1 - Iw/sum(y^2))^(2 - n/2))
+lambda = 2*pi/omega
+pl = pw * 2*pi/lambda^2
 
-library(rgl)
-plot3d(omega[,1], omega[,2], log(Iw, 10))
-plot3d(omega[,1], omega[,2], log((1 - Iw/sum(y^2))^(2 - n/2), 10))
 
-calc.post = function(omega, param){
-    beta = param[1:2]
-    v = param[3]
+omega[which.max(pw)]
+lambda[which.max(pl)]
 
-    Fm = cbind(cos(omega * tt), sin(omega * tt)) # = F'
-    exp(sum(dnorm(y, Fm %*% beta, v, log = TRUE)))
+pdf("./fp_lh.pdf", height = 8, width = 13)
+par(mfrow = c(1,2), mar = c(4.1, 4.1, 2.1, 1.1))
+plot(omega, log(pw), type='l', bty='n', main = "Frequency", ylab = "log posterior",
+    cex.main = 2.0, cex.lab = 1.5)
+plot(lambda, log(pl), type='l', bty='n', main = "Period", ylab = "log posterior",
+    cex.main = 2.0, cex.lab = 1.5)
+#plot(lambda/12, log(pl), type='l', bty='n', main = "Period (in years)", ylab = "log posterior",
+#    cex.main = 2.0, cex.lab = 1.5)
+dev.off()
 
-#   (-0.5)*determinant(Fm %*% t(Fm))$modulus[1] +
-#       ((2-n)/2)*log(1 - sum((t(beta.hat) %*% Fm)^2) / sum(y^2))
-
+# par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
+# plot(omega, pw, type='l', bty='n', main = "Frequency", ylab = "posterior")
+# plot(lambda, pl, type='l', bty='n', main = "Period", ylab = "posterior")
