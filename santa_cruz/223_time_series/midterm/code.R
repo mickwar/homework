0508d5b73afc6638833ada3e0d2e8c82d53e165e
mickey <mickey.a.warner@gmail.com>
2017-03-06 11:45:13 -0800

    yes

--- santa_cruz/223_time_series/midterm/code.R
+++ santa_cruz/223_time_series/midterm/code.R
@@ -1,13 +1,17 @@
 ### Part 1
 dat = read.csv("./googletrendsUCSC.csv")[,2]
 times = read.csv("./googletrendsUCSC.csv")[,1]
+x.ind = round(seq(1, length(dat), length = 13))
 as.numeric(times)
 
 y = diff(dat)
 n = length(y)
 tt = 1:n
 
-plot(y, type='l', bty = 'n')
+plot(y, type='l', bty = 'n', axes = FALSE, ylab = "Differences",
+    xlab = "Time", cex.lab = 1.4, cex.main = 2, main = "First-order difference of search interest")
+axis(2)
+axis(1, at = x.ind, times[x.ind])
 # axis(2)
 # ind = round(seq(1, length(y), length = 6))
 # axis(1, at = ind, labels = times[ind])
@@ -37,42 +41,12 @@ plot(lambda, log(pl), type='l', bty='n', main = "Period (in months)", ylab = "lo
 dev.off()
 
 
-## pp = pw / sum(pw)
-## cc = pl / sum(pl)
-## bb = sample(omega, 100000, replace = TRUE, prob = pp)
-## ll = 2*pi/bb
-## 
-## 
-## #pdf("./sp_lh.pdf", height = 8, width = 13)
-## par(mfrow = c(1,2), mar = c(4.1, 4.1, 2.1, 1.1))
-## yy = table(bb) / length(bb)
-## xx = as.numeric(names(yy))
-## plot(xx, as.numeric(yy), xlim = c(0, pi), type='h', bty='n',
-##     xlab = "omega", ylab = "posterior", main = "Frequency",
-##     cex.main = 2, cex.lab = 1.5)
-## lines(omega, pp, col='red')
-## 
-## yy = table(ll) / length(ll)
-## xx = as.numeric(names(yy))
-## plot(xx, as.numeric(yy), type='h', bty='n',
-##     xlab = "lambda", ylab = "posterior", main = "Period (in 10 minutes)",
-##     cex.main = 2, cex.lab = 1.5, bty='n')
-## lines(lambda, cc, col='red')
-## #dev.off()
-
 ### Part 2
-
 get.vals = function(y, delta, m0, C0, n0, d0){
     n = length(y)
-    Rt = double(n+1)
-    Qt = double(n+1)
-    At = double(n+1)
-    ft = double(n+1)
-    et = double(n+1)
-    mt = double(n+1)
-    nt = double(n+1)
-    dt = double(n+1)
-    St = double(n+1)
+    Rt = double(n+1);    Qt = double(n+1);    At = double(n+1)
+    ft = double(n+1);    et = double(n+1);    mt = double(n+1)
+    nt = double(n+1);    dt = double(n+1);    St = double(n+1)
     Ct = double(n+1)
 
     mt[1] = m0
@@ -93,32 +67,101 @@ get.vals = function(y, delta, m0, C0, n0, d0){
         Ct[i] = At[i] * St[i]
         }
     
-    return (list("Rt"=Rt, "Qt"=Qt, "At"=At,
-        "ft"=ft, "et"=et, "mt"=mt, "nt"=nt,
-        "dt"=dt, "St"=St, "Ct"=Ct))   
+    return (list("Rt"=Rt[-1], "Qt"=Qt[-1], "At"=At[-1],
+        "ft"=ft[-1], "et"=et[-1], "mt"=mt[-1], "nt"=nt[-1],
+        "dt"=dt[-1], "St"=St[-1], "Ct"=Ct[-1], "delta"=delta,
+        "m0"=m0, "C0"=C0, "n0"=n0, "d0"=d0))   
     }
 obs.pred.dens = function(dat, params){
-    df = params$nt[-length(params$nt)]
-    mu = params$ft[-1]
-    sig2 = params$Qt[-1]
+    df = c(params$n0, params$nt[-length(params$nt)])
+    mu = params$ft
+    sig2 = params$Qt
     out = lgamma((df+1)/2) - lgamma(df/2) - 1/2*log(df*pi*sig2) -
         (df+1)/2*log(1+(dat - mu)^2 / (df*sig2))
     return (sum(out))
     }
+smooth = function(dat, params){
+    n = length(dat)
+    out.at = double(n)
+    out.Rt = double(n)
+    out.at[n] = params$mt[n]
+    out.Rt[n] = params$Ct[n]
+    for (i in (n-1):1){
+        out.at[i] = params$mt[i] - params$delta*(params$mt[i] - out.at[i+1])
+        out.Rt[i] = params$Ct[i] - params$delta^2*(params$Ct[i] - out.Rt[i+1])
+        }
+    return (list("at.s"=out.at, "Rt.s"=out.Rt))
+    }
+forecast = function(dat, params, h = 12){
+    n = length(dat)
+    delta = params$delta
+    at.p = rep(params$mt[n], h)
+    Rt.p = double(h+1)
+    Rt.p[1] = params$Ct[n]
+    for (i in 2:(h-1))
+        Rt.p[i] = Rt.p[i-1] + 
+    Rt.p
 
-par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
-plot(dat, bty='n', type='l')
-out = get.vals(dat, delta = 0.8, m0 = mean(dat), C0 = var(dat), n0 = 5, d0 = 100)
+    ft.p = rep(params$mt[n], h)
 
+    C_T + (1 - d)/d * C_T = 1/d C_T
+    1/d C_T + 1/d C_T - d/d C_T
+    (2- d)/d C_T + 1/d C_T - C_T
+    
+    }
 
-plot(dat, bty='n', type='l', ylim = c(0, 100))
-lines(out$mt[-1], col = 'blue', lwd = 3)
-lines(out$mt[-1] + sqrt(out$Qt[-1])*qt(0.975, out$nt[-(n+1)]), col = 'green')
-lines(out$mt[-1] - sqrt(out$Qt[-1])*qt(0.975, out$nt[-(n+1)]), col = 'green')
-
-opd = double(11)
-del = seq(0.5, 1, length = 11)
+# Optimal discount factor
+n = length(dat)
+del = seq(0.501, 1, by = 0.001)
+m0 = dat[1]
+C0 = var(dat)
+n0 = 1
+d0 = 100
+opd = double(length(del))
 for (i in 1:length(del)){
-    out = get.vals(dat, delta = del[i], m0 = mean(dat), C0 = var(dat), n0 = 5, d0 = 100)
+    out = get.vals(dat, delta = del[i], m0 = m0, C0 = C0, n0 = n0, d0 = d0)
     opd[i] = obs.pred.dens(dat, out)
     }
+
+pdf("./figs/discount.pdf", height = 8, width = 8)
+par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
+plot(del, opd, type='l', ylab = "log observed predictive density", bty = 'n',
+    xlab = "Discount Factor" ~ delta, cex.lab = 1.4, cex.main = 2)
+abline(v = del[which.max(opd)], lty = 3, col = 'gray', lwd = 3)
+dev.off()
+(d.max = del[which.max(opd)])
+
+
+
+
+out = get.vals(dat, delta = d.max, m0 = m0, C0 = C0, n0 = n0, d0 = d0)
+par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
+
+plot(dat, bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    xlab = "Time", ylab = "Search interest", cex.lab = 1.4, cex.main = 2,
+    axes = FALSE, main = ~ y[t] ~ "|" ~ D[t-1] )
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(out$mt, col = 'blue', lwd = 4)
+lines(out$mt + sqrt(out$Qt)*qt(0.975, out$nt-1), col = 'lightblue', lwd = 2)
+lines(out$mt - sqrt(out$Qt)*qt(0.975, out$nt-1), col = 'lightblue', lwd = 2)
+
+plot(dat, bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    xlab = "Time", ylab = "Search interest", cex.lab = 1.4, cex.main = 2,
+    axes = FALSE, main = ~ theta[t] ~ "|" ~ D[t])
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(out$mt, col = 'green', lwd = 3)
+lines(out$mt + sqrt(out$Ct)*qt(0.975, out$nt), col = 'lightgreen', lwd = 2)
+lines(out$mt - sqrt(out$Ct)*qt(0.975, out$nt), col = 'lightgreen', lwd = 2)
+
+spar = smooth(dat, out)
+plot(dat, bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    cex.lab = 1.4, cex.main = 2, ylab = "Search Interest", axes = FALSE,
+    xlab = "Time", main = ~ theta[t] ~ "|" ~ D[T])
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(spar$at.s, col = 'red', lwd = 3)
+lines(spar$at.s + sqrt(spar$Rt.s*out$St[n]/out$St)*qt(0.975, out$nt[n]), col = 'pink', lwd = 2)
+lines(spar$at.s - sqrt(spar$Rt.s*out$St[n]/out$St)*qt(0.975, out$nt[n]), col = 'pink', lwd = 2)
+
