64ed17da0e279e1005e2ab291f071094cb8a6edb
mickey <mickey.a.warner@gmail.com>
2017-03-21 21:12:25 -0700

    yes

---
+++ santa_cruz/223_time_series/final/m0.R
@@ -0,0 +1,208 @@
+##### Model M0
+### Part 1
+dat = read.csv("./googletrendsUCSC.csv")[,2]
+times = read.csv("./googletrendsUCSC.csv")[,1]
+x.ind = round(seq(1, length(dat), length = 13))
+as.numeric(times)
+
+y = diff(dat)
+n = length(y)
+tt = 1:n
+
+# Plot of data
+#pdf("./figs/data.pdf", width = 14, height = 8)
+par(mfrow = c(1,2), mar = c(5.1, 4.1, 4.1, 2.1))
+plot(dat, type='o', bty = 'n', axes = FALSE, ylab = "Search Interest",
+    xlab = "Time", cex.lab = 1.4, cex.main = 2, main = "Original Data",
+    col = 'gray60')
+axis(2)
+axis(1, at = x.ind, times[x.ind])
+plot(y, type='o', bty = 'n', axes = FALSE, ylab = "Differences",
+    xlab = "Time", cex.lab = 1.4, cex.main = 2, col = 'gray60',
+    main = "First-order differences",
+    )
+axis(2)
+axis(1, at = x.ind, times[x.ind])
+#dev.off()
+
+
+
+# Spectrum
+k = 1:floor(n/2-1/2)
+omega = 2*pi*k/n
+
+a.hat = (2/n)*sapply(omega, function(w) sum(y * cos(w * tt)))
+b.hat = (2/n)*sapply(omega, function(w) sum(y * sin(w * tt)))
+Iw = (n/2)*(a.hat^2 + b.hat^2)
+pw = (1 - Iw/sum(y^2))^(1 - n/2)
+
+lambda = 2*pi/omega
+pl = pw * (2*pi/(lambda^2))^0
+
+omega[which.max(pw)]
+lambda[which.max(pl)]
+
+#pdf("./figs/spectral.pdf", width = 14, height = 8)
+par(mfrow = c(1,2), mar = c(4.1, 4.1, 2.1, 1.1))
+plot(omega, log(pw), type='l', bty='n', main = "Frequency", ylab = "log posterior",
+    cex.main = 2.0, cex.lab = 1.5)
+plot(lambda, log(pl), type='l', bty='n', main = "Period (in months)", ylab = "log posterior",
+    cex.main = 2.0, cex.lab = 1.5, xlab = "lambda", xlim = c(2, 16))
+#dev.off()
+
+
+### Part 2
+get.vals = function(y, delta, m0, C0, n0, d0){
+    n = length(y)
+    Rt = double(n+1);    Qt = double(n+1);    At = double(n+1)
+    ft = double(n+1);    et = double(n+1);    mt = double(n+1)
+    nt = double(n+1);    dt = double(n+1);    St = double(n+1)
+    Ct = double(n+1)
+
+    mt[1] = m0
+    Ct[1] = C0
+    nt[1] = n0
+    dt[1] = d0
+
+    for (i in 2:(n+1)){
+        Rt[i] = Ct[i-1] / delta # Ct[i-1] + Wt[i]
+        Qt[i] = Rt[i] + St[i-1]
+        At[i] = Rt[i]/Qt[i]   
+        ft[i] = mt[i-1]
+        et[i] = y[i-1] - ft[i]
+        mt[i] = mt[i-1] + At[i]*et[i]
+        nt[i] = nt[i-1] + 1
+        dt[i] = dt[i-1] + St[i-1]*(et[i]^2) / Qt[i]
+        St[i] = dt[i] / nt[i]
+        Ct[i] = At[i] * St[i]
+        }
+    
+    return (list("Rt"=Rt[-1], "Qt"=Qt[-1], "At"=At[-1],
+        "ft"=ft[-1], "et"=et[-1], "mt"=mt[-1], "nt"=nt[-1],
+        "dt"=dt[-1], "St"=St[-1], "Ct"=Ct[-1], "delta"=delta,
+        "m0"=m0, "C0"=C0, "n0"=n0, "d0"=d0))   
+    }
+obs.pred.dens = function(dat, params){
+    df = c(params$n0, params$nt[-length(params$nt)])
+    mu = params$ft
+    sig2 = params$Qt
+    out = lgamma((df+1)/2) - lgamma(df/2) - 1/2*log(df*pi*sig2) -
+        (df+1)/2*log(1+(dat - mu)^2 / (df*sig2))
+    return (sum(out))
+    }
+smooth = function(dat, params){
+    n = length(dat)
+    out.at = double(n)
+    out.Rt = double(n)
+    out.at[n] = params$mt[n]
+    out.Rt[n] = params$Ct[n]
+    for (i in (n-1):1){
+        out.at[i] = params$mt[i] - params$delta*(params$mt[i] - out.at[i+1])
+        out.Rt[i] = params$Ct[i] - params$delta^2*(params$Ct[i] - out.Rt[i+1])
+        }
+    return (list("at.s"=out.at, "Rt.s"=out.Rt))
+    }
+forecast = function(dat, params, h = 12){
+    n = length(dat)
+    delta = params$delta
+    at.p = rep(params$mt[n], h)
+    Rt.p = (1/delta^(1:h))*params$Ct[n]
+    ft.p = rep(params$mt[n], h)
+    Qt.p = Rt.p + params$St[n]
+    return (list("at.p"=at.p, "Rt.p"=Rt.p,
+        "ft.p"=ft.p, "Qt.p"=Qt.p))
+    }
+
+# Optimal discount factor
+n = length(dat)
+del = seq(0.500, 1, by = 0.001)
+m0 = 60
+C0 = 200
+n0 = 1
+d0 = 100
+opd = double(length(del))
+for (i in 1:length(del)){
+    out = get.vals(dat, delta = del[i], m0 = m0, C0 = C0, n0 = n0, d0 = d0)
+    opd[i] = obs.pred.dens(dat, out)
+    }
+
+#pdf("./figs/discount.pdf", height = 8, width = 8)
+par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
+plot(del, opd, type='l', ylab = "log observed predictive density", bty = 'n',
+    xlab = "Discount Factor" ~ delta, cex.lab = 1.4, cex.main = 2)
+abline(v = del[which.max(opd)], lty = 2, col = 'gray', lwd = 3)
+#dev.off()
+(d.max = del[which.max(opd)])
+
+
+
+
+out = get.vals(dat, delta = d.max, m0 = m0, C0 = C0, n0 = n0, d0 = d0)
+#pdf("./figs/current.pdf", width = 14, height = 8)
+par(mfrow = c(1,2), mar = c(5.1, 4.1, 4.1, 2.1))
+plot(dat, bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    xlab = "Time", ylab = "Search interest", cex.lab = 1.4, cex.main = 2,
+    axes = FALSE, main = ~ theta[t] ~ "|" ~ D[t])
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(out$mt, col = 'green', lwd = 3)
+lines(out$mt + sqrt(out$Ct)*qt(0.975, out$nt), col = 'lightgreen', lwd = 2)
+lines(out$mt - sqrt(out$Ct)*qt(0.975, out$nt), col = 'lightgreen', lwd = 2)
+
+plot(dat, bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    xlab = "Time", ylab = "Search interest", cex.lab = 1.4, cex.main = 2,
+    axes = FALSE, main = ~ y[t] ~ "|" ~ D[t-1] )
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(out$mt, col = 'blue', lwd = 4)
+lines(out$mt + sqrt(out$Qt)*qt(0.975, out$nt-1), col = 'lightblue', lwd = 2)
+lines(out$mt - sqrt(out$Qt)*qt(0.975, out$nt-1), col = 'lightblue', lwd = 2)
+#dev.off()
+
+spar = smooth(dat, out)
+#pdf("./figs/smooth.pdf", width = 8, height = 8)
+par(mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1))
+plot(dat, bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    cex.lab = 1.4, cex.main = 2, ylab = "Search Interest", axes = FALSE,
+    xlab = "Time", main = ~ theta[t] ~ "|" ~ D[T])
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(spar$at.s, col = 'red', lwd = 3)
+lines(spar$at.s + sqrt(spar$Rt.s*out$St[n]/out$St)*qt(0.975, out$nt[n]), col = 'pink', lwd = 2)
+lines(spar$at.s - sqrt(spar$Rt.s*out$St[n]/out$St)*qt(0.975, out$nt[n]), col = 'pink', lwd = 2)
+#dev.off()
+
+
+h = 12
+ppar = forecast(dat, out, h)
+#pdf("./figs/forecast.pdf", width = 14, height = 8)
+par(mfrow = c(1,2), mar = c(5.1, 4.1, 4.1, 2.1))
+plot(1:(n+h), c(dat, rep(NA, h)), bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    cex.lab = 1.4, cex.main = 2, ylab = "Search Interest", axes = FALSE,
+    xlab = "Time", main = ~ theta[T+h] ~ "|" ~ D[T])
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(out$mt, col = 'green', lwd = 3)
+lines(out$mt + sqrt(out$Ct)*qt(0.975, out$nt), col = 'lightgreen', lwd = 2)
+lines(out$mt - sqrt(out$Ct)*qt(0.975, out$nt), col = 'lightgreen', lwd = 2)
+lines((n+1):(n+h), ppar$at.p, col = 'darkgreen', lwd = 3)
+lines((n+1):(n+h), ppar$at.p + sqrt(ppar$Rt.p)*qt(0.975, out$nt[n]),
+    col = 'darkgreen', lwd = 2)
+lines((n+1):(n+h), ppar$at.p - sqrt(ppar$Rt.p)*qt(0.975, out$nt[n]),
+    col = 'darkgreen', lwd = 2)
+
+plot(1:(n+h), c(dat, rep(NA, h)), bty='n', type='o', ylim = c(0, 120), col = 'gray60',
+    xlab = "Time", ylab = "Search interest", cex.lab = 1.4, cex.main = 2,
+    axes = FALSE, main = ~ y[T+h] ~ "|" ~ D[T] )
+axis(2)
+axis(1, at = x.ind, labels = times[x.ind])
+lines(out$mt, col = 'blue', lwd = 4)
+lines(out$mt + sqrt(out$Qt)*qt(0.975, out$nt-1), col = 'lightblue', lwd = 2)
+lines(out$mt - sqrt(out$Qt)*qt(0.975, out$nt-1), col = 'lightblue', lwd = 2)
+lines((n+1):(n+h), ppar$ft.p, col = 'darkblue', lwd = 3)
+lines((n+1):(n+h), ppar$ft.p + sqrt(ppar$Qt.p)*qt(0.975, out$nt[n]),
+    col = 'darkblue', lwd = 2)
+lines((n+1):(n+h), ppar$at.p - sqrt(ppar$Qt.p)*qt(0.975, out$nt[n]),
+    col = 'darkblue', lwd = 2)
+#dev.off()
+
--- santa_cruz/263_stoch_proc/proj/code_3.R
+++ santa_cruz/263_stoch_proc/proj/code_3.R
@@ -1,10 +1,14 @@
 library(mwBASE)
 tmp = read.table("./california_pr_obs19501999.txt", header = TRUE)
 
+
 tt = tmp[,1]
 y = tmp[,2]
 n = length(y)
 nyears = 50
+
+#tt = 1:18262
+#y = rnorm(length(tt))
 
 
 # tmp = substr(tt, 5, 10)
@@ -35,34 +39,59 @@ make.X = function(x, p = 1){
     return (out)
     }
 
-p = 1
+p = 36
 X = make.X(xx, p)
 bhat = solve(t(X) %*% X) %*% t(X) %*% log(qq)
 pred.x = 1:n
 pred.y = exp(make.X(pred.x, p) %*% bhat)
-plot(y, type='l', xlim = c(1, 1000))
-lines(xx, qq, col = 'red')
-lines(pred.x, pred.y, col = 'darkgreen', lwd = 3)
-abline(v = seq(1, 365, length = 13), lty = 2, col = 'gray')
 
-plot(y - pred.y, xlim = c(0, 1500))
+rm.leap = grep("-02-29", tt)
+daily.10 = aggregate(y[-rm.leap], list(substr(tt[-rm.leap], 5, 10)), function(x) quantile(x, 0.93))
+qq = daily.10$x
+p = 6
+X = make.X(1:365, p)
+bhat = solve(t(X) %*% X) %*% t(X) %*% log(qq)
+pred.x = 1:n
+pred.y = exp(make.X(pred.x, p) %*% bhat)
+
+plot(qq)
+lines(pred.x, pred.y)
+
+# pred.y = double(n)
+# tmp.ind = grep("(-12-)|(-01-)|(-02-)", tt)
+# pred.y[tmp.ind] = quantile(y[tmp.ind], 0.95)
+# tmp.ind = grep("(-03-)|(-04-)|(-05-)", tt)
+# pred.y[tmp.ind] = quantile(y[tmp.ind], 0.95)
+# tmp.ind = grep("(-06-)|(-07-)|(-08-)", tt)
+# pred.y[tmp.ind] = quantile(y[tmp.ind], 0.95)
+# tmp.ind = grep("(-09-)|(-10-)|(-11-)", tt)
+# pred.y[tmp.ind] = quantile(y[tmp.ind], 0.95)
+
+plot(y, type='l', xlim = c(1, 2000))
+plot(y, type='l', xlim = c(n-2000, n))
+#lines(xx, qq, col = 'red')
+lines(pred.x, pred.y, col = 'darkgreen', lwd = 3)
+abline(v = 365*seq(0, 49), lty=2, col = 'gray')
+#abline(v = seq(1, 365, length = 13), lty = 2, col = 'gray')
+
+
+plot(y - pred.y, xlim = c(0, 2500))
 abline(h = 0)
 ind = which(y - pred.y > 0)
 plot(density(y[ind]))
 
+
 dat = list("y" = y, "threshold" = pred.y, "ind" = ind, "nyears" = nyears,
     "exceed" = (y - pred.y)[ind])
 
-
-# should be approximately uniform
-hist(ind / n)
-
-
-
+# proportion of exceedances
 length(ind) / n
 
+# should be approximately uniform
+hist(ind / n, col = 'gray', breaks = 20)
+
 # number of exceedances by day/month
-plot(table(substr(tt[ind], 5, 10)) / length(ind))
+plot(table(substr(tt[ind], 5, 10)) / length(ind), pch = 16, type='l')
 
 # number of exceedances by month
 plot(table(substr(tt[ind], 5, 8)) / length(ind))
@@ -71,6 +100,47 @@ plot(table(substr(tt[ind], 5, 8)) / length(ind))
 plot(table(substr(tt[ind], 1, 4)) / length(ind))
 
 
+calc.like = function(dat, params){
+    y = dat$y[dat$ind]
+    u_vec = dat$threshold[dat$ind]
+    n = length(dat$y)
+    tt = (1:n)[dat$ind]
+    nyears = dat$nyears
+
+    # mu
+    mu_int = params[1]
+    mu_a = params[2]
+    mu_b = params[3]
+
+    # sigma
+    sig_int = params[4]
+    sig_a = params[5]
+    sig_b = params[6]
+
+    # ksi
+    ksi_int = params[7]
+    ksi_a = params[8]
+    ksi_b = params[9]
+    ksi_vec = ksi_int + ksi_a*cos(2*pi/365.25 * tt) + ksi_b*sin(2*pi/365.25 * tt)
+#   ksi_vec = rep(ksi_int, length(tt))
+
+    mu_vec = mu_int + mu_a*cos(2*pi/365.25 * tt) + mu_b*sin(2*pi/365.25 * tt)
+    sig_vec = exp(sig_int + sig_a*cos(2*pi/365.25 * tt) + sig_b*sin(2*pi/365.25 * tt))
+    
+    ksi_vec = ifelse(ksi_vec == 0, 1e-10, ksi_vec)
+
+    A = (1 + ksi_vec*(u_vec - mu_vec)/sig_vec)
+    B = (1 + ksi_vec*(y - mu_vec)/sig_vec)
+
+    if (min(A) <= 0) return (-Inf)
+    if (min(B) <= 0) return (-Inf)
+
+
+    # likelihood
+    out = sum(-nyears * A^(-1/ksi_vec)-log(sig_vec)-(1/ksi_vec+1)*log(B))
+
+    return (out)
+    }
 
 calc.post = function(dat, params){
     y = dat$y[dat$ind]
@@ -93,10 +163,11 @@ calc.post = function(dat, params){
     ksi_int = params[7]
     ksi_a = params[8]
     ksi_b = params[9]
+    ksi_vec = ksi_int + ksi_a*cos(2*pi/365.25 * tt) + ksi_b*sin(2*pi/365.25 * tt)
+#   ksi_vec = rep(ksi_int, length(tt))
 
     mu_vec = mu_int + mu_a*cos(2*pi/365.25 * tt) + mu_b*sin(2*pi/365.25 * tt)
     sig_vec = exp(sig_int + sig_a*cos(2*pi/365.25 * tt) + sig_b*sin(2*pi/365.25 * tt))
-    ksi_vec = ksi_int + ksi_a*cos(2*pi/365.25 * tt) + ksi_b*sin(2*pi/365.25 * tt)
     
     ksi_vec = ifelse(ksi_vec == 0, 1e-10, ksi_vec)
 
@@ -120,8 +191,8 @@ calc.post = function(dat, params){
     out = out + dnorm(sig_b, 0, 100, log = TRUE)
 
     out = out + dnorm(ksi_int, 0, 100, log = TRUE)
-    out = out + dnorm(ksi_a, 0, 100, log = TRUE)
-    out = out + dnorm(ksi_b, 0, 100, log = TRUE)
+#   out = out + dnorm(ksi_a, 0, 100, log = TRUE)
+#   out = out + dnorm(ksi_b, 0, 100, log = TRUE)
 
     return (out)
     }
@@ -144,6 +215,7 @@ sig_vec = apply(out$params[,4:6], 1,
     function(p) exp(p[1] + p[2]*cos(2*pi/365.25*1:365) + p[3]*sin(2*pi/365.25*1:365)))
 ksi_vec = apply(out$params[,7:9], 1,
     function(p) p[1] + p[2]*cos(2*pi/365.25*1:365) + p[3]*sin(2*pi/365.25*1:365))
+#ksi_vec = matrix(rep(out$params[,7], each = 365), 365)
 
 tvar_mu = rbind(apply(mu_vec, 1, mean), apply(mu_vec, 1, quantile, c(0.025, 0.975)))
 tvar_sig = rbind(apply(sig_vec, 1, mean), apply(sig_vec, 1, quantile, c(0.025, 0.975)))
@@ -184,6 +256,7 @@ for (i in 1:k){
         out$params[,6]*sin(2*pi/365.25*ind[i]))
     tmp_ksi = out$params[,7] + out$params[,8]*cos(2*pi/365.25*ind[i]) +
         out$params[,9]*sin(2*pi/365.25*ind[i])
+#   tmp_ksi = out$params[,7]
     tmp_squig = tmp_sig + tmp_ksi*(pred.y[ind[i]] - tmp_mu)
 
     stand_y[i,] = 1/tmp_ksi*log(1 + tmp_ksi*(y[ind[i]] - pred.y[ind[i]])/tmp_squig)
@@ -209,79 +282,151 @@ lines(-log(1-(1:k)/(k+1)), y_qq2[1, ord])
 lines(-log(1-(1:k)/(k+1)), y_qq2[2, ord])
 abline(0, 1)
 
+# DIC
+d.theta = apply(out$params, 1, function(p) calc.like(dat, p))
+mean(d.theta) + 0.5*var(d.theta)
+mean(d.theta)
+0.5*var(d.theta)
+
 
 ### Return levels
-# 5-, 10-, 20-, 50-year at the 15th of every month.
-mid = round(seq(15, 350, length = 12))
-zeta = matrix(0, nrow(out$params), 365)
+u.year = exp(make.X(1:365, p) %*% bhat)
+#u.year = pred.y[1:365]
+#zeta = matrix(0, 365, nrow(out$params))
+#for (i in 1:365)
+#    zeta[i,] = 1/365*(1 + ksi_vec[i,]*(u.year[i] - mu_vec[i,])/sig_vec[i,])^(-1/ksi_vec[i,])
 
+get_zm = function(m){
+    z = matrix(0, nrow(out$params), 365)
+    for (i in 1:365){
+        tmp_squig = sig_vec[i,] + ksi_vec[i,] * (u.year[i] - mu_vec[i,])
+        z[,i] =  u.year[i] + tmp_squig/ksi_vec[i,]*( (m^ksi_vec[i,]) - 1)
+#       z[,i] =  u.year[i] + tmp_squig/ksi_vec[i,]*( (zeta[i,]*m/365)^(ksi_vec[i,]) - 1)
+#       z[,i] =  mu_vec[i,] + sig_vec[i,]/ksi_vec[i,]*( (m/1)^(ksi_vec[i,]) - 1)
+        }
+    return (z)
+    }
+
+z.10 = get_zm(50*1)
+z.mm = apply(z.10, 2, mean)
+z.qq = apply(z.10, 2, quantile, c(0.025, 0.975))
+rm.leap = grep("-02-29", tt)
+daily.10 = aggregate(y[-rm.leap], list(substr(tt[-rm.leap], 5, 10)), function(x) tail(sort(x), 10))
+
+plot(z.mm, pch = 16, ylim = c(0, max(z.10, y)))
+segments(1:365, z.qq[1,], 1:365, z.qq[2,])
 for (i in 1:365)
-    zeta[,i] = (1 + ksi_vec[i,] * 
-        (pred.y[i] - mu_vec[i,]) / sig_vec[i,]) ^ (-1/ksi_vec[i,])
+    points(rep(i, 10), unlist(daily.10[i,][2]), col = rainbow(10), pch = 16)
+lines(daily.10$x[,10])
+abline(v = seq(1, 365, length = 13), lty = 2, col = 'gray')
 
-range(colMeans(zeta))
+tmp = sort(z.10[,c(1:59, 335:365)])
+tmp = tail(tmp, nrow(z.10))
+t.mm = mean(tmp)
+t.qq = quantile(tmp, c(0.025, 0.975))
+lines(1:59, rep(t.mm, 59), col = 'blue', lwd = 3)
+lines(335:365, rep(t.mm, 31), col = 'blue', lwd = 3)
+lines(1:59, rep(t.qq[1], 59), col = 'blue')
+lines(335:365, rep(t.qq[1], 31), col = 'blue')
+lines(1:59, rep(t.qq[2], 59), col = 'blue')
+lines(335:365, rep(t.qq[2], 31), col = 'blue')
 
+tmp = sort(z.10[,152:243])
+tmp = tail(tmp, nrow(z.10))
+t.mm = mean(tmp)
+t.qq = quantile(tmp, c(0.025, 0.975))
+lines(152:243, rep(t.mm, 92), col = 'red', lwd = 3)
+lines(152:243, rep(t.qq[1], 92), col = 'red')
+lines(152:243, rep(t.qq[2], 92), col = 'red')
 
+tmp = sort(z.10[,60:151])
+tmp = tail(tmp, nrow(z.10))
+t.mm = mean(tmp)
+t.qq = quantile(tmp, c(0.025, 0.975))
+lines(60:151, rep(t.mm, 92), col = 'green', lwd = 3)
+lines(60:151, rep(t.qq[1], 92), col = 'green')
+lines(60:151, rep(t.qq[2], 92), col = 'green')
 
-
-plot(pred.y[1:365])
-
-
-mu = out$params[,1]
-sig = out$params[,2]
-ksi = out$params[,3]
-zeta = rbeta(length(mu), y$n_u + 1, y$n - y$n_u + 1)
-
-squig = sig + ksi*(y$threshold - mu)
-
-plot(density(mu))
-plot(density(squig))
-plot(density(ksi))
-
-
-param.star = cbind(ksi, squig, zeta)
-return.period = sort(unique(c(20, 30, 50, exp(seq(log(0.1), log(100), length = 100)))))
-
-ZZ = apply(param.star, 1,
-    function(x)
-        y$threshold[1] + x[2]/x[1] * (((return.period*ndays)*x[3])^x[1] - 1)
-    )
-Zm = apply(ZZ, 1, mean)
-Zq = apply(ZZ, 1, quantile, c(0.025, 0.975))
-
-color = "dodgerblue"
-
-par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1))
-xaxis = NULL
-for (i in floor(log(0.1, 10)):(ceiling(log(100, 10))-1))
-    xaxis = c(xaxis, seq(1, 9, by = 1)*10^i)
-xaxis = c(xaxis, 10^ceiling(log(100, 10)))
-xlab = xaxis
-xlab[-seq(1, length(xaxis), by = 9)] = NA
-plot(log(return.period), Zm, ylim = range(Zq),
-    type='n', axes = FALSE,
-    xlab = paste0("Return Period (# of winters)"),
-    ylab = paste0("Return Level (m/days)"),
-#   main = "Mean Return Level Plot", col = col_mult(output$base.color, 'gray50'),
-        cex.lab = 1.2, cex.main = 1.5)
-axis(1, at = log(xaxis), labels = xlab); axis(2)
-
-polygon(c(log(return.period), rev(log(return.period))),
-    c(Zq[1,], rev(Zq[2,])), col = col_fade(color, 0.5),
-    border = NA)
-lines(log(return.period), Zq[1,],
-    col = col_fade(color, 0.5), lwd = 2, lend = 1)
-lines(log(return.period), Zq[2,],
-    col = col_fade(color, 0.5), lwd = 2, lend = 1)
-
-points(log(tail(1/((y$n:1)/(y$n+1)) / ndays, y$n_u)),
-    y$threshold + sort(y$exceed), pch = 16,
-    col = col_fade("black", 0.7), type='o')
+tmp = sort(z.10[,244:334])
+tmp = tail(tmp, nrow(z.10))
+t.mm = mean(tmp)
+t.qq = quantile(tmp, c(0.025, 0.975))
+lines(244:334, rep(t.mm, 91), col = 'orange', lwd = 3)
+lines(244:334, rep(t.qq[1], 91), col = 'orange')
+lines(244:334, rep(t.qq[2], 91), col = 'orange')
 
 
-
-
-pred = (runif(length(squig))^(-ksi) - 1) * squig / ksi
-hist(y$exceed, col = 'gray', breaks = 20, freq = FALSE)
-lines(density(pred, n = 10000), col = 'green', lwd = 3)
-
+## # 5-, 10-, 20-, 50-year at the 15th of every month.
+## mid = round(seq(15, 350, length = 12))
+## zeta = matrix(0, nrow(out$params), 365)
+## 
+## for (i in 1:365)
+##     zeta[,i] = (1 + ksi_vec[i,] * 
+##         (pred.y[i] - mu_vec[i,]) / sig_vec[i,]) ^ (-1/ksi_vec[i,])
+## 
+## range(colMeans(zeta))
+## 
+## 
+## 
+## 
+## plot(pred.y[1:365])
+## 
+## 
+## mu = out$params[,1]
+## sig = out$params[,2]
+## ksi = out$params[,3]
+## zeta = rbeta(length(mu), y$n_u + 1, y$n - y$n_u + 1)
+## 
+## squig = sig + ksi*(y$threshold - mu)
+## 
+## plot(density(mu))
+## plot(density(squig))
+## plot(density(ksi))
+## 
+## 
+## param.star = cbind(ksi, squig, zeta)
+## return.period = sort(unique(c(20, 30, 50, exp(seq(log(0.1), log(100), length = 100)))))
+## 
+## ZZ = apply(param.star, 1,
+##     function(x)
+##         y$threshold[1] + x[2]/x[1] * (((return.period*ndays)*x[3])^x[1] - 1)
+##     )
+## Zm = apply(ZZ, 1, mean)
+## Zq = apply(ZZ, 1, quantile, c(0.025, 0.975))
+## 
+## color = "dodgerblue"
+## 
+## par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1))
+## xaxis = NULL
+## for (i in floor(log(0.1, 10)):(ceiling(log(100, 10))-1))
+##     xaxis = c(xaxis, seq(1, 9, by = 1)*10^i)
+## xaxis = c(xaxis, 10^ceiling(log(100, 10)))
+## xlab = xaxis
+## xlab[-seq(1, length(xaxis), by = 9)] = NA
+## plot(log(return.period), Zm, ylim = range(Zq),
+##     type='n', axes = FALSE,
+##     xlab = paste0("Return Period (# of winters)"),
+##     ylab = paste0("Return Level (m/days)"),
+## #   main = "Mean Return Level Plot", col = col_mult(output$base.color, 'gray50'),
+##         cex.lab = 1.2, cex.main = 1.5)
+## axis(1, at = log(xaxis), labels = xlab); axis(2)
+## 
+## polygon(c(log(return.period), rev(log(return.period))),
+##     c(Zq[1,], rev(Zq[2,])), col = col_fade(color, 0.5),
+##     border = NA)
+## lines(log(return.period), Zq[1,],
+##     col = col_fade(color, 0.5), lwd = 2, lend = 1)
+## lines(log(return.period), Zq[2,],
+##     col = col_fade(color, 0.5), lwd = 2, lend = 1)
+## 
+## points(log(tail(1/((y$n:1)/(y$n+1)) / ndays, y$n_u)),
+##     y$threshold + sort(y$exceed), pch = 16,
+##     col = col_fade("black", 0.7), type='o')
+## 
+## 
+## 
+## 
+## pred = (runif(length(squig))^(-ksi) - 1) * squig / ksi
+## hist(y$exceed, col = 'gray', breaks = 20, freq = FALSE)
+## lines(density(pred, n = 10000), col = 'green', lwd = 3)
+## 
