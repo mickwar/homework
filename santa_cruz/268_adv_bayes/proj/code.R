845899d7801424350636ee0a9ca893fc0f9e43f9
mickey <mickey.a.warner@gmail.com>
2018-03-06 11:46:50 -0800

    yes

--- santa_cruz/268_adv_bayes/proj/code.R
+++ santa_cruz/268_adv_bayes/proj/code.R
@@ -10,10 +10,10 @@ make.sigma = function(k, rho){
     return (out)
     }
 ### Normal Inverse Gamma density
-dnig = function(beta, sig2, mu, lambda, a, b, log = TRUE){
+dnig = function(beta, sig2, mu, lambdainv, a, b, log = TRUE){
     k = length(mu)
     out = -(a + k/2 + 1)*log(sig2) - (1/sig2) *
-        (b + 1/2*t(beta - mu) %*% lambda %*% (beta - mu))
+        (b + 1/2*t(beta - mu) %*% lambdainv %*% (beta - mu))
     if (log)
         return (out)
     return (exp(out))
@@ -58,7 +58,7 @@ n = 10^6    # no. data points   (10^8)
 m = 100     # no. of subsamples, subsets    (1000)
             # Make sure n/m is an integer and n/m > k
 
-k = 40     # no. of predictors  (100)
+k = 40      # no. of predictors  (100)
 rho = 0.99  # corr. between predictors (0.99)
 sigma = 10  # measurement error
 
@@ -69,7 +69,7 @@ set.seed(1)
 for (i in 1:m){
     X = mvrnorm(n/m, rep(0, k), sig.mat)
     y = rnorm(n/m, mean = X %*% beta, sd = sigma)
-    fname = paste0(c(DATA_DIR, "dat_", rep("0", nchar(m) - nchar(i)), i, ".txt"), collapse = "")
+    fname = paste0(c(DATA_DIR, "dat_", rho, "_", rep("0", nchar(m) - nchar(i)), i, ".txt"), collapse = "")
     write.table(round(data.frame(y, X), 3), quote = FALSE, row.names = FALSE, file = fname)
     rm(X, y)
     gc()
@@ -78,19 +78,23 @@ for (i in 1:m){
 
 ### Read in data and calculate posterior distributions
 ### for each data subset
-m = length(list.files(DATA_DIR))
-fname = paste0(c(DATA_DIR, "dat_", rep("0", nchar(m) - nchar(1)), 1, ".txt"), collapse = "")
+#m = length(list.files(DATA_DIR))
+m = 100
+fname = paste0(c(DATA_DIR, "dat_", rho, "_", rep("0", nchar(m) - nchar(1)), 1, ".txt"), collapse = "")
 z = read.table(fname, header = TRUE)
 n = NROW(z)*m
 k = NCOL(z) - 1
 rm(z)
 
+
 mu.s = matrix(0, k, m)
-lambda.s = rep(list(matrix(0, k, k)), m)
+#lambda.s = rep(list(matrix(0, k, k)), m)
+lambda.s = array(0, c(m, k, k))
+
 a.s = double(m)
 b.s = double(m)
 for (i in 1:m){
-    fname = paste0(c(DATA_DIR, "dat_", rep("0", nchar(m) - nchar(i)), i, ".txt"), collapse = "")
+    fname = paste0(c(DATA_DIR, "dat_", rho, "_", rep("0", nchar(m) - nchar(i)), i, ".txt"), collapse = "")
     z = read.table(fname, header = TRUE)
     y = as.vector(z[,1])
     X = as.matrix(z[,-1])
@@ -98,13 +102,13 @@ for (i in 1:m){
     xty = t(X) %*% y
     yty = sum(y^2)
 
-    lambda.s[[i]] = t(X) %*% X
+    lambda.s[i,,] = t(X) %*% X
     lam.inv = solve(t(X) %*% X)
     mu.s[,i] = lam.inv %*% xty
     a.s[i] = (n/m - k) / 2
     b.s[i] = 1/2 * (yty - t(xty) %*% lam.inv %*% xty)
 
-    rm(X, y, z)
+    rm(X, y, z, xty, yty)
     gc()
     }
 
@@ -112,12 +116,12 @@ for (i in 1:m){
 ### Calculate full posterior (sum of the subsets)
 # (Algorithm 1)
 mu = mu.s[,1]
-lambda = lambda.s[[1]]
+lambda = lambda.s[1,,]
 a = a.s[1]
 b = b.s[1]
 
 for (i in 2:m){
-    tmp = nig_sum(mu, lambda, a, b, mu.s[,i], lambda.s[[i]], a.s[i], b.s[i])
+    tmp = nig_sum(mu, lambda, a, b, mu.s[,i], lambda.s[i,,], a.s[i], b.s[i])
     mu = tmp$mu
     lambda = tmp$lambda
     a = tmp$a
@@ -275,40 +279,18 @@ points(1:k, beta, col = 'firebrick', pch = 15)
 plot_hpd(draws.sig2)
 
 
+
+
 ### MC^3
 # (Algorithm 4)
-# d = 0.01^2
-D = 100
-
-#prior.mu = rep(0, k)
-#prior.lam = 1/D * diag(k)
-
-# Beta prior for w (mixture weight)
-# a.w = rep(1, k)
-# b.w = rep(1, k)
-w.vec = rep(0.5, k)
-
-
-# Gibbs set up
-nburn = 2000
-nmcmc = 5000
-B = nburn + nmcmc
-# draws.beta = matrix(0, B, k)
-# draws.sig2 = double(B)
-draws.gamma = matrix(0, B, k)   # Indicators for selected variables
-# draws.w = matrix(0, B, k)       # Probabilities for indicators
-accept = double(B)
-
-#draws.sig2[1] = 1
-#draws.gamma[1,] = rep(0, k)     # Start with no selected variables
-draws.gamma[1,] = rbinom(k, 1, prob = 0.5)     # Start with randomly selected variables
-# draws.w[1,] = rep(0.5, k)
-
 calc.post = function(gamma, mu, lambda, a, b){
     vg = (gamma == 1)
     vo = (gamma == 0)
     kg = sum(vg)
     ko = sum(vo)
+
+    if (kg == 0 || ko == 0)
+        return (-Inf)
 
     # 4.2, reduce
     sig2 = 1
@@ -319,20 +301,21 @@ calc.post = function(gamma, mu, lambda, a, b){
     lam.gg = matrix(lambda[vg, vg], kg, kg)
     lam.go = matrix(lambda[vg, vo], kg, ko)
     lam.og = matrix(lambda[vo, vg], ko, kg)
-    lam.oo = matrix(lambda[vo, vg], ko, ko)
+    lam.oo = matrix(lambda[vo, vo], ko, ko)
 
 #   # gamma subscripts
-#    mu.squig = mu.g + solve(lam.gg) %*% lam.go %*% mu.o
-#   lam.squig = lam.gg
-#     a.squig = a + ko / 2
-#     b.squig = b + 1/2 * t(mu.o) %*% (lam.oo - lam.og %*% solve(lam.gg) %*% lam.go) %*% mu.o
-     mu.squig = mu.g
+     mu.squig = mu.g + solve(lam.gg) %*% lam.go %*% mu.o
     lam.squig = lam.gg
-      a.squig = a
-      b.squig = b
+      a.squig = a + ko / 2
+      b.squig = b + 1/2 * t(mu.o) %*% (lam.oo - lam.og %*% solve(lam.gg) %*% lam.go) %*% mu.o
+#    mu.squig = mu.g
+#   lam.squig = lam.gg
+#     a.squig = a
+#     b.squig = b
 
     mu.prior = rep(0, kg)
-    lam.prior = 1/D * diag(kg)
+#   lam.prior = 1/D * diag(kg)
+    lam.prior = 1/c * XtX[vg, vg]
     a.prior = 0
     b.prior = 0 
     
@@ -344,38 +327,84 @@ calc.post = function(gamma, mu, lambda, a, b){
     a.bar = tmp$a
     b.bar = tmp$b
     
-    return (dnig(rep(0, kg), sig2, mu.prior, lam.prior, a.prior, b.prior) -
-        dnig(rep(0, kg), sig2, mu.bar, lam.bar, a.bar, b.bar) +
+    return (dnig(rep(0, kg), sig2, mu.prior, solve(lam.prior), a.prior, b.prior) -
+        dnig(rep(0, kg), sig2, mu.bar, solve(lam.bar), a.bar, b.bar) +
         sum(dbinom(gamma, 1, prob = w.vec, log = TRUE)))
-        dbeta(gamma,)
     }
+
+# d = 0.01^2
+D = 100
+c = D
+
+#prior.mu = rep(0, k)
+#prior.lam = 1/D * diag(k)
+
+# Beta prior for w (mixture weight)
+# a.w = rep(1, k)
+# b.w = rep(1, k)
+#w.vec = rep(0.5, k)
+
+
+# Gibbs set up
+nburn = 5000
+nmcmc = 20000
+B = nburn + nmcmc
+# draws.beta = matrix(0, B, k)
+# draws.sig2 = double(B)
+draws.gamma = matrix(0, B, k)   # Indicators for selected variables
+# draws.w = matrix(0, B, k)       # Probabilities for indicators
+accept = double(B)
+
+#draws.sig2[1] = 1
+#draws.gamma[1,] = rep(0, k)     # Start with no selected variables
+#draws.gamma[1,] = rbinom(k, 1, prob = 0.5)     # Start with randomly selected variables
+#draws.gamma[1,] = rep(1, k)     # Start with no selected variables
+draws.gamma[1,] = c(1, rep(0, k-1))
+# draws.w[1,] = rep(0.5, k)
+
+XtX = apply(lambda.s, c(2,3), sum)
+
+# for AIC
+w = 1 / (1 + 1/sqrt(1+c) * exp(2*c/(2*(1+c))))
+
+# for BIC
+#w = 1 / (1 + 1/sqrt(1+c) * exp(log(n)*c/(2*(1+c))))
+
+w.vec = rep(w, k)
+
+
 
 curr.post = calc.post(draws.gamma[1,], mu, lambda, a, b)
 
 
 for (i in 2:B){
     draws.gamma[i,] = draws.gamma[i-1,]
+    if (i %% 100 == 0)
+        cat(i, "/", B, "\r")
 
     # 4.1, propose gamma
-    gamma.prop = draws.gamma[i-1,]
+    cand.gamma = draws.gamma[i-1,]
     tmp.ind = sample(k, 1)
-    gamma.prop[tmp.ind] = 1 - gamma.prop[tmp.ind]
+    cand.gamma[tmp.ind] = 1 - cand.gamma[tmp.ind]
 
-    cand.post = calc.post(gamma.prop, mu, lambda, a, b)
+    cand.post = calc.post(cand.gamma, mu, lambda, a, b)
 
     if (log(runif(1)) <= cand.post - curr.post){
-        draws.gamma[i,] = gamma.prop
+        draws.gamma[i,] = cand.gamma
         curr.post = cand.post
         accept[i] = 1
         }
 
+    if (i == B)
+        cat("\n")
     }
 
+draws.gamma = tail(draws.gamma, nmcmc)
+accept = tail(accept, nmcmc)
+
 mean(accept)
-sum(accept)
 draws.gamma[1,]
 draws.gamma[7000,]
 
-plot(draws.gamma[,16])
 colMeans(draws.gamma)
-# try with no correlated data
+
